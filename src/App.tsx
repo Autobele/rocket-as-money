088885fbfc067fff5a49b687170c0986e9039c17
Autobele <autobele@infoway-pi.com.br>
2021-08-25 00:42:20 -0300

    Add assets and configure global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
+const Title = styled.h1`
+  
+`
+
 export function App() {
   return (
     <div className="App">
+      <Title>Hello World</Title>
+      <GlobalStyle/>
     </div>
   );
 }
