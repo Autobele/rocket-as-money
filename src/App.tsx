9eb108565f759453ea2839cd6b6b305d2ce68722
Autobele <autobele@infoway-pi.com.br>
2021-08-26 21:57:37 -0300

    Add transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header/index'
 import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
+import Modal from 'react-modal';
 
 export function App() {
+
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false) ;
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle/>
     </>
   );
